ef6ca578a2f766fdb9212ec5ae26b131c5b1214b
kibirige <kibirigejohn005@gmail.com>
2022-12-03 19:51:32 +0300

    add specs for comment and like models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_count
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_comment_counter
+  private
 
-  def update_posts_counter
-    post.update(comments_counter: post.comments_counter + 1)
+  def update_comments_count
+    post.update(comments_counter: post.comments.count)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,11 @@
 class Like < ApplicationRecord
+  after_save :update_likes_count
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_likes_counter
+  private
 
-  def update_likes_counter
-    post.update(likes_counter: post.likes_counter + 1)
+  def update_likes_count
+    post.update(likes_counter: post.likes.count)
   end
 end
