3ea0fcaa156f290a3c801283f42c07d15181699b
kibirige <kibirigejohn005@gmail.com>
2022-12-03 16:07:58 +0300

    add specs for the user model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comment_counter
+
   post.update(comments_counter: post.comments_counter + 1)
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_likes_counter
+
   post.update(likes_counter: post.likes_counter + 1)
 end
