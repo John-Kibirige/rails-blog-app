7ae90aa7ee2db7bf30253c03c9341c3b9136fced
kibirige <kibirigejohn005@gmail.com>
2022-12-07 10:01:38 +0300

    setup a posts controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   root "users#index"
 
   get 'users', to: "users#index"
   get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
 end
