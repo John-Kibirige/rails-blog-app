25af58924d637f371bc1d95386381e90185670c9
kibirige <kibirigejohn005@gmail.com>
2022-12-11 00:15:08 +0300

    allow user to add comment and create a like

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'likes/create'
   root 'users#index'
 
   get "users/:user_id/posts/new", to: "posts#new"
   post "users/:user_id/posts", to: "posts#create"
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+   resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
-
 
 end
