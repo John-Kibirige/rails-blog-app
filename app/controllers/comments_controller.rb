debb98ffa301b05d17b84fab0677eec8590e471c
kibirige <kibirigejohn005@gmail.com>
2022-12-11 01:55:36 +0300

    fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
-
   def new
     @new_comment = Comment.new
   end
+
   def create
     post = Post.find(params[:post_id])
     all_comments = params[:comment]
-    Comment.create(post: post, author: current_user, text: all_comments["comment"])
+
+    Comment.create(post: post, author: current_user, text: all_comments['comment'])
 
     redirect_to user_posts_path, notice: 'Successfully created post'
   end
