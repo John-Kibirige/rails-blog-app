616f9999ee4770b3df21d9b69b38ccc332865b71
kibirige <kibirigejohn005@gmail.com>
2022-12-10 21:41:37 +0300

    add routes for handling the post addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  get "users/:user_id/posts/new", to: "posts#new"
+  post "users/:user_id/posts", to: "posts#create"
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
+
+
 end
