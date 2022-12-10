eca50e270d61739d2443620ac4b2243f3480f8f1
kibirige <kibirigejohn005@gmail.com>
2022-12-10 21:40:58 +0300

    add extra actions in the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,21 @@ class PostsController < ApplicationController
   end
 
   def show
+    puts params[:id]
     @post = Post.find(params[:id])
   end
+
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    all_posts = params[:post]
+
+    Post.create(author: current_user, title: all_posts["title"], text: all_posts["text"])
+
+    redirect_to user_posts_path, notice: "Successfully created post"
+  end
+
+
 end
