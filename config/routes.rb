2d615d95bb38ee7a3959dc4b4437721573bb7663
kibirige <kibirigejohn005@gmail.com>
2022-12-12 16:19:41 +0300

    add routes for adding posts,likes and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   get 'likes/create'
   root 'users#index'
 
-  get 'users/:user_id/posts/new', to: 'posts#new'
-  post 'users/:user_id/posts', to: 'posts#create'
-
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show] do
-      resources :comments, only: %i[new create]
-      resources :likes, only: [:create]
+  resources :users do
+    resources :posts do
+      resources :comments
+      resources :likes
     end
   end
 end
