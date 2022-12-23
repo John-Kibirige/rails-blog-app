3727cdea94bcbab8dab379aa71069d1366a0d956
kibirige <kibirigejohn005@gmail.com>
2022-12-23 12:00:04 +0300

    add devise to the project and carry out authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if user_signed_in?
+      @users = User.all
+
+    else
+      redirect_to new_user_session_path
+  end
+
   end
 
   def show
